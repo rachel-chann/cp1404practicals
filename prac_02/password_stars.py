e5f5ec3400941b68379d444947c7ce45b3b70012
rachel-chann <rachel.chan1@my.jcu.edu.au>
2022-09-22 13:12:27 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,27 @@
+"""
+CP1404 Prac 2
+Password Star
+"""
+
 MIN_LENGTH = 6
-password = input("password: ")
-while len(password) < MIN_LENGTH:
-    print(f"The password should be consisted of at least {MIN_LENGTH} characters.")
+
+
+def main():
+    """Print asterisks as long as the input password."""
+    password = get_password()
+    print_asterisks(password)
+
+
+def get_password():
     password = input("password: ")
-print(f'password: {len(password) * "*"}')
+    while len(password) < MIN_LENGTH:
+        print(f"The password should be consisted of at least {MIN_LENGTH} characters.")
+        password = input("password: ")
+    return password
+
+
+def print_asterisks(password):
+    print(f'password: {len(password) * "*"}')
+
+
+main()
