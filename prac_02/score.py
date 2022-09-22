7c18679ec2de1c722b14fc52c94796ec5fbce7b7
rachel-chann <rachel.chan1@my.jcu.edu.au>
2022-09-22 14:28:38 +1000

    Refactor score status determination program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,29 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+from random import randint
+MIN_SCORE = 0
+MAX_SCORE = 100
+
+
+def main():
+    """Print score status."""
+    score = float(input("Enter score: "))
+    print(determine_score(score))
+    random_score = randint(0, 100)
+    print(determine_score(random_score))
+
+
+def determine_score(score):
+    """Determine score status."""
+    if score < MIN_SCORE or score > MAX_SCORE:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
