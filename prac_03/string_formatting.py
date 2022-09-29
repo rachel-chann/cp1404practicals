7aecb70728e61f46f3d1ad3a4e1fcbeab39e72f0
rachel-chann <rachel.chan1@my.jcu.edu.au>
2022-09-29 16:23:28 +1000

    Add string formatting program

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -1,5 +1,5 @@
 """
-CP1404/CP5632 - Practical
+CP1404/CP5632 - String Formatting
 Various examples of using Python string formatting.
 (We prefer f-strings in this subject.)
 Want to read more about it?
